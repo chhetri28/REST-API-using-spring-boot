9af1b0f3122562f3049b3a0c6682efe7a728a3bc
chhetri28 <chettrisourav28@gmail.com>
2023-02-11 14:05:40 +0530

    All the request is sent

---
+++ restapi/src/main/java/com/example/restapi/controller/MyController.java
@@ -0,0 +1,41 @@
+package com.example.restapi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.restapi.entities.Course;
+import com.example.restapi.services.CourseService;
+
+@RestController
+public class MyController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @GetMapping("/home")
+    public String home() {
+        return "This is home page";
+    }
+    //get all courses
+    @GetMapping("/courses")
+    public List<Course> getCourses(){
+       return this.courseService.getCourses();
+    }
+
+    //get single course
+    @GetMapping("/courses/{courseId}")
+    public Course getCourse(@PathVariable String courseId){
+        return this.courseService.getCourse(Long.parseLong(courseId));
+    }
+
+    @PostMapping("/courses")
+    public Course addCourse(@RequestBody Course course){
+        return this.courseService.addCourse(course);
+    }
+}

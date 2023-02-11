a52383589bfd70651f10e99496dcd378af0d61ea
chhetri28 <chettrisourav28@gmail.com>
2023-02-11 18:40:48 +0530

    Dao class implemented

--- restapi/src/main/java/com/example/restapi/entities/Course.java
+++ restapi/src/main/java/com/example/restapi/entities/Course.java
@@ -1,6 +1,14 @@
 package com.example.restapi.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private String description;
--- restapi/src/main/java/com/example/restapi/services/CourseServiceImpl.java
+++ restapi/src/main/java/com/example/restapi/services/CourseServiceImpl.java
@@ -1,58 +1,73 @@
 package com.example.restapi.services;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.restapi.dao.CourseDao;
 import com.example.restapi.entities.Course;
 
 @Service
 public class CourseServiceImpl implements CourseService {
-
-    List<Course>list;
-    public CourseServiceImpl(){
-        list= new ArrayList<>();
-        list.add(new Course(145,"Java Core Course","this course for java"));
-        list.add(new Course(147, "Java Spring Course", "this course for Spring introduction"));
-        list.add(new Course(149, "Java SpringBoot Course", "this course for SpringBoot introduction"));
-    }
+    
+    @Autowired
+    private CourseDao courseDao;
+    
+    /*
+     * List<Course>list;
+     * public CourseServiceImpl(){
+     * list= new ArrayList<>();
+     * list.add(new Course(145,"Java Core Course","this course for java"));
+     * list.add(new Course(147, "Java Spring Course",
+     * "this course for Spring introduction"));
+     * list.add(new Course(149, "Java SpringBoot Course",
+     * "this course for SpringBoot introduction"));
+     * }
+     */
     @Override
     public List<Course> getCourses() {
-
-        return list;
+        return courseDao.findAll();
     }
     @Override
     public Course getCourse(long courseId) {
-        Course c=null;
-        for(Course course:list){
-            if(course.getId()==courseId){
-                c=course;
-                break;
-            }
-        }
-        return c;
+        /*
+         * Course c=null;
+         * for(Course course:list){
+         * if(course.getId()==courseId){
+         * c=course;
+         * break;
+         * }
+         * }
+         */
+        return courseDao.getReferenceById(courseId);
     }
     @Override
     public Course addCourse(Course course) {
-        list.add(course);
+       // list.add(course);
+       courseDao.save(course);
         return course;
     }
     @Override
     public Course updateCourse(Course course) {
-        list.forEach(e->{
-            if(e.getId()==course.getId()){
-                e.setTitle(course.getTitle());
-                e.setDescription(course.getDescription());
-            }
-        });
+        /*
+         * list.forEach(e->{
+         * if(e.getId()==course.getId()){
+         * e.setTitle(course.getTitle());
+         * e.setDescription(course.getDescription());
+         * }
+         * });
+         */
+        courseDao.save(course);
         return course;
     }
     @Override
     public void deleteCourse(long courseId) {
-        list=this.list.stream().filter(e->e.getId()!=courseId)
-        .collect(Collectors.toList());
+        /*
+         * list=this.list.stream().filter(e->e.getId()!=courseId)
+         * .collect(Collectors.toList());
+         */
+        Course entity= courseDao.getReferenceById(courseId);
+        courseDao.delete(entity);
     }
     
     

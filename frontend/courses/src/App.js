d5c302ee11e48e946096b5ab7ebac8197db05025
chhetri28 <chettrisourav28@gmail.com>
2023-03-25 22:03:56 +0530

    Button from reactstrap

---
+++ frontend/courses/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Button } from 'reactstrap';
+function App() {
+  return (
+    <div>
+      <h1>This is first react button</h1>
+      <Button>First React Button</Button>
+    </div>
+  );
+}
+
+export default App;
